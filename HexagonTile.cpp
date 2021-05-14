9c0ab03fe9994e6aeb90671111cf423697841042
Amir Mohammad <tavakkoli.amirmohammad@gmail.com>
2021-05-14 12:42:35 +0430

    Add render method to hexagon tile

--- HexagonTile.cpp
+++ HexagonTile.cpp
@@ -1,5 +1,28 @@
 #include "HexagonTile.h"
 
-HexagonTile::HexagonTile(HexagonTexture *hexagonTexture) : hexagonTexture_(hexagonTexture) {
+HexagonTile::HexagonTile(HexagonTexture *hexagonTexture, int size) : hexagonTexture_(hexagonTexture), size_(size) {
+
+}
+
+void HexagonTile::render() const {
+    auto tx_w = static_cast<float >(1.0 / size_);
+    auto tx_h = static_cast<float >(1.0 / size_);
+
+    glBindTexture(GL_TEXTURE_2D, hexagonTexture_->getTextureId());
+
+    glBegin(GL_POLYGON);
+    glTexCoord2f(0, 0);
+    glVertex2f(0, 0);
+
+    glTexCoord2f(1.0, 0);
+    glVertex2f(tx_w, 0);
+
+    glTexCoord2f(1.0, 1.0);
+    glVertex2f(tx_w, tx_h);
+
+    glTexCoord2f(0, 1.0);
+    glVertex2f(0, tx_h);
+
+    glEnd();
 
 }
--- HexagonTile.h
+++ HexagonTile.h
@@ -5,10 +5,13 @@
 
 class HexagonTile {
 public:
-    explicit HexagonTile(HexagonTexture *hexagonTexture);
+    HexagonTile(HexagonTexture *hexagonTexture, int size);
+
+    void render() const;
 
 private:
     HexagonTexture *hexagonTexture_;
+    int size_;
 };
 
 #endif //MAP_GENERATION_HEXAGONTILE_H
